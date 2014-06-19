9c6449ab231cb3d6f0a076104822a85b74a4841d
Arnab Deka <arnab.deka@gmail.com>
2014-06-19 08:27:59 +0200

    stm example.

--- stm/src/stm/core.clj
+++ stm/src/stm/core.clj
@@ -1,6 +1,16 @@
 (ns stm.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def transfer-count (atom 0))
+
+(let [fn-positive #(>= % 0)]
+  (def alice (ref 1000 :validator fn-positive))
+  (def bob (ref 2000 :validator fn-positive)))
+
+(defn transfer [from to amount]
+  (dosync
+   (swap! transfer-count inc)
+   (alter from - amount)
+   (alter to + amount)))
+
+#_ (repeatedly 25 #(transfer alice bob 100))
+#_ (println[@alice @bob @transfer-count])
